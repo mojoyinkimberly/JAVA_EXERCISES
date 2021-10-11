a4e3e6ed2c3eaac402545514a28065ee5a1abf5d
Mojoyinola Oketope <m.oketope@semicolon.africa>
2021-10-12 00:22:33 +0100

    ...

--- OTHER_EXERCISES/studentGrade.java
+++ OTHER_EXERCISES/studentGrade.java
@@ -1,32 +1,32 @@
-//tell user to input score
-//if studentScore >= 60,print pass
-//if studentScore < 60,print fail
-
-import java.util.Scanner;
-public class studentScore{
-
-	public static void main(String[] args){
-	
-	Scanner input = new Scanner(System.in);
-	
-		int score;
-		
-	System.out.println("Enter score");
-	
-		score = input.nextInt();
-		
-
-	if(score >= 60){System.out.println("pass");
-	
-}
-	else{System.out.println("fail");
-	
-}
-
-
-
-}
-
-
-}
-
+////tell user to input score
+////if studentScore >= 60,print pass
+////if studentScore < 60,print fail
+//
+//import java.util.Scanner;
+//public class studentScore{
+//
+//	public static void main(String[] args){
+//
+//	Scanner input = new Scanner(System.in);
+//
+//		int score;
+//
+//	System.out.println("Enter score");
+//
+//		score = input.nextInt();
+//
+//
+//	if(score >= 60){System.out.println("pass");
+//
+//}
+//	else{System.out.println("fail");
+//
+//}
+//
+//
+//
+//}
+//
+//
+//}
+//
