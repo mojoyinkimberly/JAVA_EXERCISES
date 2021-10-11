1a90fd1c6d6627b281f35673a01a1a431192bef9
Mojoyinola Oketope <m.oketope@semicolon.africa>
2021-10-12 00:17:22 +0100

    ...

--- CHAPTER_9_EXERCISES/BillingInformation (another copy).java
+++ CHAPTER_9_EXERCISES/BillingInformation (another copy).java
@@ -1,9 +1,9 @@
-package chapterNine;
-
-public class BillingInformation {
-    private String receiverPhone;
-    private String receiverName;
-    private Address deliveryAddress;
-    private CreditCardInformation creditCardInformation;
-
-}
+//package chapterNine;
+//
+//public class BillingInformation {
+//    private String receiverPhone;
+//    private String receiverName;
+//    private Address deliveryAddress;
+//    private CreditCardInformation creditCardInformation;
+//
+//}
