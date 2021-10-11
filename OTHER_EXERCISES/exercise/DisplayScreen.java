43d73b4c84034bb9a3d3d966098bc3a469778797
Mojoyinola Oketope <m.oketope@semicolon.africa>
2021-10-12 00:18:51 +0100

    ...

---
+++ OTHER_EXERCISES/exercise/DisplayScreen.java
@@ -0,0 +1,12 @@
+package exercise;
+
+import java.util.Scanner;
+
+public class DisplayScreen {
+    public static void main(String[] args) {
+        SevenSegmentDisplay.collectUserInput();
+        SevenSegmentDisplay.setScreen();
+        SevenSegmentDisplay.display();
+
+    }
+}
