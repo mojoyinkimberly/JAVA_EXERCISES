48d2b74915ec4096509c7b338deeff89f4eaad9c
Mojoyinola Oketope <m.oketope@semicolon.africa>
2021-10-12 00:22:04 +0100

    ...

---
+++ OTHER_EXERCISES/exercise/SevenSegmentDisplayTest.java
@@ -0,0 +1,13 @@
+package exercise;
+
+import org.junit.jupiter.api.Test;
+
+public class SevenSegmentDisplayTest {
+    @Test
+    public void testThatSevenSegmentDisplayCanPrintCorrectly(){
+        //given
+        SevenSegmentDisplay sevenSegmentDisplay= new SevenSegmentDisplay();
+        //when
+
+    }
+}
